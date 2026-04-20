0db86f466110e3eb6c1113f28a8dfa4188ed6e44
jeandsc <jeancampos147@gmail.com>
2026-04-20 07:42:48 -0300

    Teste para static int

--- tests/test_parser.rs
+++ tests/test_parser.rs
@@ -26,4 +26,36 @@ mod tests {
 </class>"#;
         assert_eq!(parser.get_xml(), expected);
     }
+    #[test]
+fn test_parse_class_with_static_int() {
+    let tokens = vec![
+        Token { kind: TokenType::CLASS, lexeme: "class".to_string(), line: 1 },
+        Token { kind: TokenType::IDENT, lexeme: "Main".to_string(), line: 1 },
+        Token { kind: TokenType::LBRACE, lexeme: "{".to_string(), line: 1 },
+        Token { kind: TokenType::STATIC, lexeme: "static".to_string(), line: 2 },
+        Token { kind: TokenType::INT, lexeme: "int".to_string(), line: 2 },
+        Token { kind: TokenType::IDENT, lexeme: "x".to_string(), line: 2 },
+        Token { kind: TokenType::SEMICOLON, lexeme: ";".to_string(), line: 2 },
+        Token { kind: TokenType::RBRACE, lexeme: "}".to_string(), line: 3 },
+        Token { kind: TokenType::EOF, lexeme: "".to_string(), line: 4 },
+    ];
+
+    let mut parser = Parser::new(tokens);
+    parser.parse_code();
+
+    let expected = r#"<class>
+  <keyword> class </keyword>
+  <identifier> Main </identifier>
+  <symbol> { </symbol>
+  <classVarDec>
+    <keyword> static </keyword>
+    <keyword> int </keyword>
+    <identifier> x </identifier>
+    <symbol> ; </symbol>
+  </classVarDec>
+  <symbol> } </symbol>
+</class>"#;
+
+    assert_eq!(parser.get_xml(), expected);
+}
 }
